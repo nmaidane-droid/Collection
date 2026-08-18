29283328af902d10dac4589d9f699d8c6ed8a5c9
nmaidane-droid <nmaidane@hotmail.com>
2026-08-18 02:14:27 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    MÉRIDIENNE — service worker
    À CHAQUE MODIFICATION D'UN FICHIER, INCRÉMENTER VERSION
    ======================================================= */
-const VERSION = 'silhouette-v5';
+const VERSION = 'silhouette-v9';
 
 const COQUE = [
   './',
